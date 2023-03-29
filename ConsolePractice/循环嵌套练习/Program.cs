fde640d1beea62471a104a75568453e3c0807869
ruaruachou <dddplay123@gmail.com>
2023-03-29 16:12:29 +0800

    3月29日

---
+++ ConsolePractice/循环嵌套练习/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Lottery
+{
+    class LotteryApp 
+    {
+       private static void Main()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write(" ");
+                   
+                }
+                for (int k = 0; k < 4-i; k++)
+                {
+                    Console.Write("#");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
