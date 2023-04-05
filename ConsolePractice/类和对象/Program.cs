9671c16f1ec15edccb11ee383c01a350f06dade6
ruaruachou <dddplay123@gmail.com>
2023-04-06 04:19:01 +0800

    类和对象练习

---
+++ ConsolePractice/类和对象/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace SecondCount
+{
+    class Wife
+    {
+        //数据成员
+        private string name;
+        private string sex;
+        private int age;
+
+        //方法成员
+        private void SetName(string name)
+        {
+            this.name = name;
+        }
+        private void SetAge(int age)
+        {
+            this.age = age;
+        }
+        public string GetName()
+        {
+            return name; 
+        }
+        public int GetAge()
+        {
+            return age;
+        }
+        static void Main()
+        {
+            Wife beautyfulWife;
+            beautyfulWife = new Wife();//实例化wifu对象
+            beautyfulWife.SetName("新垣结衣");
+            beautyfulWife.SetAge(34);
+
+            Console.WriteLine(beautyfulWife.GetName());
+            Console.WriteLine(beautyfulWife.GetAge());
+        }
+    }
+}
